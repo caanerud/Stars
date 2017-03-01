b0f758b2024608b62fa4592999fe7dbc9ac45965
Forest Newark <forest.newark@gmail.com>
2017-03-01 17:03:52 -0500

    Added code for stars

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,19 @@
  * Created by chrisaanerud on 3/1/17.
  */
 public class Main {
+    public static void main(String[] args) {
+        int height = 10;
+        for (int rows = 1; rows <= height;rows++) {
+            for (int whiteSpaces =1;whiteSpaces <= (height-rows);whiteSpaces++) {
+                System.out.print(" ");
+            }
+            for (int stars = 1;stars <=(rows * 2 - 1);stars++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
 }
